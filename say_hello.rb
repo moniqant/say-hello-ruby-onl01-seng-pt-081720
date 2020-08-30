cc328fcd59a5d3b8d91a9b9a932e25c13ec537b3
remote-glitch-3651 <moniquedantoine17@gmail.com>
2020-08-30 21:43:02 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,17 @@
 # Build your say_hello method here
 # I call on the method, say_hello, and give it the string "Gabriela" 
+<<<<<<< HEAD
 
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
 end 
 
  
+=======
+learn submitsay_hello("Gabriela")
+end
+
+# I call on the method, say_hello, and give it the string "" 
+say_hello()
+end
+>>>>>>> cb9668b20dae2c69370fd20f035f638daf081a6e
